6ae0d5e0a10822887320304a492bfa6f2da7b7ba
abishek <abishek@alpenlabs.io>
2025-01-03 11:47:07 +0545

    example: sha2 chain example

---
+++ artifacts/sp1/sha2-chain/src/main.rs
@@ -0,0 +1,6 @@
+use sha2_chain::process_sha_chain;
+use strata_sp1_adapter::Sp1ZkVmEnv;
+
+fn main() {
+    process_sha_chain(&Sp1ZkVmEnv)
+}
--- runner/src/programs/mod.rs
+++ runner/src/programs/mod.rs
@@ -1,11 +1,13 @@
 use std::str::FromStr;
 
 pub mod fibonacci;
+pub mod sha2;
 
 #[derive(Debug)]
 #[non_exhaustive]
 pub enum TestProgram {
     Fibonacci,
+    Sha2Chain,
 }
 
 impl FromStr for TestProgram {
@@ -14,6 +16,7 @@ impl FromStr for TestProgram {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s.to_lowercase().as_str() {
             "fibonacci" => Ok(TestProgram::Fibonacci),
+            "sha2-chain" => Ok(TestProgram::Sha2Chain),
             // Add more matches
             _ => Err(format!("unknown program: {}", s)),
         }
