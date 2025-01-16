46b3faf6b2fb6b7a0307e7d5b1048f4b1e1e1d58
Prajwol Gyawali <prajwol@alpenlabs.io>
2025-01-16 16:24:14 +0545

    use entrypoint macro for sp1

--- artifacts/sp1/fibonacci/src/main.rs
+++ artifacts/sp1/fibonacci/src/main.rs
@@ -1,3 +1,10 @@
+// These two lines are necessary for the program to properly compile.
+//
+// Under the hood, we wrap your main function with some extra code so that it behaves properly
+// inside the zkVM.
+#![no_main]
+sp1_zkvm::entrypoint!(main);
+
 use fibonacci::process_fib;
 use strata_sp1_adapter::Sp1ZkVmEnv;
 
--- artifacts/sp1/sha2-chain/src/main.rs
+++ artifacts/sp1/sha2-chain/src/main.rs
@@ -1,6 +1,13 @@
+// These two lines are necessary for the program to properly compile.
+//
+// Under the hood, we wrap your main function with some extra code so that it behaves properly
+// inside the zkVM.
+#![no_main]
+sp1_zkvm::entrypoint!(main);
+
 use sha2_chain::process_sha_chain;
 use strata_sp1_adapter::Sp1ZkVmEnv;
 
-fn main() {
+pub fn main() {
     process_sha_chain(&Sp1ZkVmEnv)
 }
