0138e7537ca17778c211e79f3ced70094e49b8b1
Prajwol Gyawali <prajwol@alpenlabs.io>
2025-01-28 18:12:10 +0545

    update composition program

--- runner/src/programs/fibonacci_composition.rs
+++ runner/src/programs/fibonacci_composition.rs
@@ -1,23 +1,35 @@
 use fibonacci::FibProver;
-use zkaleido::{ProofReport, ZkVmHost, ZkVmProver};
+use fibonacci_composition::FibCompositionProver;
+use zkaleido::{AggregationInput, ProofReport, ZkVmHost, ZkVmProver};
 
-fn fib_composition_prover_perf_report(host: &impl ZkVmHost) -> ProofReport {
+fn fib_composition_prover_perf_report(
+    fib_host: &impl ZkVmHost,
+    fib_composition_host: &impl ZkVmHost,
+) -> ProofReport {
     let input = 5;
-    FibProver::perf_report(&input, host).unwrap()
+    let receipt = FibProver::prove(&input, fib_host).unwrap();
+    println!("{:?}", receipt);
+    let vk = fib_host.get_verification_key();
+    let input = AggregationInput::new(receipt, vk);
+    FibCompositionProver::perf_report(&input, fib_composition_host).unwrap()
 }
 
 #[cfg(feature = "sp1")]
 pub fn sp1_fib_composition_report() -> ProofReport {
     use zkaleido_sp1_adapter::SP1Host;
-    use zkaleido_sp1_artifacts::FIBONACCI_ELF;
-    let host = SP1Host::init(FIBONACCI_ELF);
-    fib_composition_prover_perf_report(&host)
+    use zkaleido_sp1_artifacts::{FIBONACCI_COMPOSITION_ELF, FIBONACCI_ELF};
+    let fib_host = SP1Host::init(FIBONACCI_ELF);
+    let fib_composition_host = SP1Host::init(FIBONACCI_COMPOSITION_ELF);
+    fib_composition_prover_perf_report(&fib_host, &fib_composition_host)
 }
 
 #[cfg(feature = "risc0")]
 pub fn risc0_fib_composition_report() -> ProofReport {
     use zkaleido_risc0_adapter::Risc0Host;
-    use zkaleido_risc0_artifacts::GUEST_RISC0_FIBONACCI_ELF;
-    let host = Risc0Host::init(GUEST_RISC0_FIBONACCI_ELF);
-    fib_composition_prover_perf_report(&host)
+    use zkaleido_risc0_artifacts::{
+        GUEST_RISC0_FIBONACCI_COMPOSITION_ELF, GUEST_RISC0_FIBONACCI_ELF,
+    };
+    let fib_host = Risc0Host::init(GUEST_RISC0_FIBONACCI_ELF);
+    let fib_composition_host = Risc0Host::init(GUEST_RISC0_FIBONACCI_COMPOSITION_ELF);
+    fib_composition_prover_perf_report(&fib_host, &fib_composition_host)
 }
