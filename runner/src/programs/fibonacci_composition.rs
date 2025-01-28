757854420d66c031b02dd5bfd4354aebd85d9781
Prajwol Gyawali <prajwol@alpenlabs.io>
2025-01-28 17:17:06 +0545

    add initial support for fibonacci composition in runner

---
+++ runner/src/programs/fibonacci_composition.rs
@@ -0,0 +1,23 @@
+use fibonacci::FibProver;
+use zkaleido::{ProofReport, ZkVmHost, ZkVmProver};
+
+fn fib_composition_prover_perf_report(host: &impl ZkVmHost) -> ProofReport {
+    let input = 5;
+    FibProver::perf_report(&input, host).unwrap()
+}
+
+#[cfg(feature = "sp1")]
+pub fn sp1_fib_composition_report() -> ProofReport {
+    use zkaleido_sp1_adapter::SP1Host;
+    use zkaleido_sp1_artifacts::FIBONACCI_ELF;
+    let host = SP1Host::init(FIBONACCI_ELF);
+    fib_composition_prover_perf_report(&host)
+}
+
+#[cfg(feature = "risc0")]
+pub fn risc0_fib_composition_report() -> ProofReport {
+    use zkaleido_risc0_adapter::Risc0Host;
+    use zkaleido_risc0_artifacts::GUEST_RISC0_FIBONACCI_ELF;
+    let host = Risc0Host::init(GUEST_RISC0_FIBONACCI_ELF);
+    fib_composition_prover_perf_report(&host)
+}
