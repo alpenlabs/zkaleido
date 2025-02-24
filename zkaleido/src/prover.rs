f486c038e8f351d85db50e5419b657abd27ef257
Prajwol Gyawali <prajwol@alpenlabs.io>
2025-02-24 08:42:52 +0545

    feat: add ZkVmRemoteProver trait

--- zkaleido/src/prover.rs
+++ zkaleido/src/prover.rs
@@ -1,5 +1,7 @@
 use std::fmt::Debug;
 
+use async_trait::async_trait;
+
 use crate::{
     input::ZkVmInputBuilder, ProofReceipt, ProofType, PublicValues, ZkVmError, ZkVmProofError,
     ZkVmResult,
@@ -49,5 +51,49 @@ pub trait ZkVmProver: ZkVmExecutor {
     ) -> ZkVmResult<ProofReceipt> {
         let receipt = self.prove_inner(input, proof_type)?;
         receipt.try_into().map_err(ZkVmError::InvalidProofReceipt)
+    }
+}
+
+/// A trait implemented by the prover of a zkVM program.
+///
+/// This trait extends [`ZkVmProver`] to support asynchronous remote proving operations.
+/// It provides methods to start the proving process and retrieve the proof once it
+/// becomes available. Implementers of this trait typically handle the remote communication
+/// required to generate and fetch proofs.
+#[async_trait(?Send)]
+pub trait ZkVmRemoteProver: ZkVmProver {
+    /// Starts the proving process for the given input and proof type.
+    ///
+    /// This method typically initiates the remote proof generation and returns
+    /// a proof identifier (`proof_id`) that can be used to query the status
+    /// of the proof later.
+    async fn start_proving<'a>(
+        &self,
+        input: <Self::Input<'a> as ZkVmInputBuilder<'a>>::Input,
+        proof_type: ProofType,
+    ) -> ZkVmResult<String>;
+
+    /// Retrieves the proof if it is ready.
+    ///
+    /// This method performs the underlying logic to check whether the proof
+    /// has been generated and is available from the remote service. If it is
+    /// not ready yet, `None` will be returned.
+    async fn get_proof_if_ready_inner(
+        &self,
+        id: String,
+    ) -> ZkVmResult<Option<Self::ZkVmProofReceipt>>;
+
+    /// Retrieves the proof if it is ready and converts it into a [`ProofReceipt`].
+    ///
+    /// Internally calls [`get_proof_if_ready_inner`](Self::get_proof_if_ready_inner)
+    /// to fetch the proof receipt and attempts to convert it into a
+    /// [`ProofReceipt`]. If the proof is not ready, `None` is returned.
+    async fn get_proof_if_ready(&self, id: String) -> ZkVmResult<Option<ProofReceipt>> {
+        let receipt = self.get_proof_if_ready_inner(id).await?;
+        let res = match receipt {
+            Some(inner_receipt) => Some(inner_receipt.try_into()?),
+            None => None,
+        };
+        Ok(res)
     }
 }
