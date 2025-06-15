fcdb329e35cb5e9d30887691dc47756eea231a28
Prajwol Gyawali <prajwol@alpenlabs.io>
2025-06-15 20:43:05 +0545

    Feat/sp1 groth16 verifier (#49)
    * port error and types from sp1-verifier
    
    * feat(converter): deserialize to fq
    
    * convert from gnark to bn compressed g1 bytes
    
    * feat: working loading groth16 vk
    
    * fix: working g2 point, g1 not working
    
    * fix g1
    
    * use consistent logic for compressed gnark bytes to g1 and g2
    
    * remove constants file
    
    * refactor: cleanups and organization
    
    * refactor: add Sp1Verifier
    
    * docs: add docstring and comments
    
    * better docs and refs
    
    * feat: add borsh serialization to types
    
    * feat: implement Debug for VK
    
    * fix: avoid panics

---
+++ adapters/sp1/groth16-verifier/src/verification.rs
@@ -0,0 +1,44 @@
+use bn::{pairing_batch, Fr, Gt, G1, G2};
+
+use crate::{
+    error::Groth16Error,
+    types::{Groth16Proof, Groth16VerifyingKey},
+};
+
+/// Prepare the inputs for the Groth16 verification by combining the public inputs with the
+/// corresponding elements of the verification key.
+fn prepare_inputs(vk: Groth16VerifyingKey, public_inputs: &[Fr]) -> Result<G1, Groth16Error> {
+    if (public_inputs.len() + 1) != vk.g1.k.len() {
+        return Err(Groth16Error::PrepareInputsFailed);
+    }
+    Ok(public_inputs
+        .iter()
+        .zip(vk.g1.k.iter().skip(1))
+        .fold(G1::from(vk.g1.k[0]), |acc, (i, b)| {
+            acc + (G1::from(*b) * (*i))
+        }))
+}
+
+/// Verify the Groth16 proof using algebraic inputs.
+///
+/// First, prepare the public inputs by folding them with the verification key.
+/// Then, verify the proof by checking the pairing equation.
+pub(crate) fn verify_groth16_algebraic(
+    vk: &Groth16VerifyingKey,
+    proof: &Groth16Proof,
+    public_inputs: &[Fr],
+) -> Result<(), Groth16Error> {
+    let prepared_inputs = prepare_inputs(vk.clone(), public_inputs)?;
+
+    if pairing_batch(&[
+        (-Into::<G1>::into(proof.ar), proof.bs.into()),
+        (prepared_inputs, vk.g2.gamma.into()),
+        (proof.krs.into(), vk.g2.delta.into()),
+        (vk.g1.alpha.into(), -Into::<G2>::into(vk.g2.beta)),
+    ]) == Gt::one()
+    {
+        Ok(())
+    } else {
+        Err(Groth16Error::ProofVerificationFailed)
+    }
+}
